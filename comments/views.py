d3ca50267c956898c31028f9883a9c0497a23936
Illia <akhtyrtsev@gmail.com>
2019-07-10 21:55:12 +0300

    added sorting and validation

--- comments/views.py
+++ comments/views.py
@@ -11,7 +11,6 @@ from django.core.paginator import Paginator
 from user_agents import parse
 
 class AddComment(View):
-
     @staticmethod
     def byName(comment):
         return comment.user_name
@@ -20,10 +19,7 @@ class AddComment(View):
         return comment.published_date
     def sortFunction(self, comments, value):
         new_comment = [comment for comment in comments if len(comment.path)==4]
-        print('new_comment', new_comment)
-        print('new_comment_value', value)
         children_comment = [comment for comment in comments if len(comment.path)!=4]
-        print('children_comment', children_comment)
         if value == '1':
             sorted_new_comment = sorted(new_comment, key=self.byName)
         elif value == '2':
@@ -32,7 +28,6 @@ class AddComment(View):
             sorted_new_comment = sorted(new_comment, key=self.byDate)
         elif value == '4':
             sorted_new_comment = sorted(new_comment, key=self.byDate, reverse = True)
-        print('sorted_new_comment', sorted_new_comment )
         counter = 0
         result = []
         for comment in sorted_new_comment:
@@ -42,11 +37,10 @@ class AddComment(View):
                 if comment.path[1:] in child.path:
                     result.append(child)
                     counter +=1
-        print('before returning', result)
         return result
     def post(self, request):
         if request.method == 'POST':
-            form = CommentForm(request.POST)
+            form = CommentForm(request.POST) 
             if form.is_valid():
                 comment = form.save(commit=False)
                 comment.path = ''
@@ -62,20 +56,19 @@ class AddComment(View):
                 except:
                     comment.path += '1%(number)03d' % {'number':comment.id}
                 comment.save()
-            return HttpResponseRedirect('/comments/')
-    def get(self, request):
+            return self.get(request, form)
+
+    def get(self, request, form=CommentForm()): 
+        print(form.errors)
         result_list = Comment.objects.order_by('path')
         criterion = '4'
         if request.GET.get('sort'):
             criterion = request.GET.get('sort')
-        print('Criterion', criterion)
-        print('before filtering', result_list)
         result_list = self.sortFunction(result_list, criterion)
-        print('after filtering', result_list)
         paginator = Paginator(result_list, 4) 
         page = request.GET.get('page')
         results = paginator.get_page(page)
-        return render(request, "comments/index.html", {"results": results, "form":CommentForm})
+        return render(request, "comments/index.html", {"results": results, "form":form})
 
     
 
